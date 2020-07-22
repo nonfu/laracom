6e392d51b21cdd486683af7bad3d7607508048a6
nonfu <lvbailvzhanzhe@gmail.com>
2020-07-22 23:10:48 +0800

    基于 k8s 启动 demo 服务

--- demo-service/go.mod
+++ demo-service/go.mod
@@ -3,32 +3,15 @@ module github.com/nonfu/laracom/demo-service
 go 1.12
 
 require (
-	github.com/coreos/etcd v3.3.18+incompatible // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.3.5
-	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/golang/protobuf v1.4.0
 	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.9.5 // indirect
-	github.com/lucas-clemente/quic-go v0.14.1 // indirect
 	github.com/micro/go-micro v1.18.0
-	github.com/micro/go-plugins v1.5.1
-	github.com/miekg/dns v1.1.27 // indirect
-	github.com/nats-io/nats-server/v2 v2.1.6 // indirect
-	github.com/nonfu/laracom/common v0.0.0-20200426051241-f74ec06b51dd
+	github.com/micro/go-micro/v2 v2.9.1
+	github.com/micro/kubernetes v0.7.0
+	github.com/nonfu/laracom/common v0.0.0-20200426051241-f74ec06b51dd // indirect
 	github.com/nonfu/laracom/user-service v0.0.0-20200410143932-a6f4d5f4b264
-	github.com/opentracing/opentracing-go v1.1.0
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sirupsen/logrus v1.5.0
+	github.com/sirupsen/logrus v1.5.0 // indirect
 	github.com/smartystreets/assertions v1.0.1 // indirect
 	github.com/smartystreets/goconvey v1.6.4
-	github.com/tmc/grpc-websocket-proxy v0.0.0-20200122045848-3419fae592fc // indirect
-	go.etcd.io/bbolt v1.3.4 // indirect
-	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
-	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	golang.org/x/tools v0.0.0-20191216173652-a0e659d51361 // indirect
-	google.golang.org/genproto v0.0.0-20191216164720-4f79533eabd1 // indirect
-	google.golang.org/grpc v1.26.0 // indirect
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2
 )
--- demo-service/main.go
+++ demo-service/main.go
@@ -3,51 +3,20 @@ package main
 import (
 	"context"
 	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/config"
-	"github.com/micro/go-micro/config/encoder/json"
-	"github.com/micro/go-micro/config/source"
-	"github.com/micro/go-micro/config/source/etcd"
-	"github.com/micro/go-micro/config/source/file"
-	"github.com/micro/go-micro/metadata"
-	traceplugin "github.com/micro/go-plugins/wrapper/trace/opentracing"
-	"github.com/nonfu/laracom/common/tracer"
-	"github.com/nonfu/laracom/common/wrapper/breaker/hystrix"
+	k8s "github.com/micro/kubernetes/go/micro"
 	pb "github.com/nonfu/laracom/demo-service/proto/demo"
 	userpb "github.com/nonfu/laracom/user-service/proto/user"
-	"github.com/opentracing/opentracing-go"
-	"github.com/sirupsen/logrus"
-	_ "github.com/nonfu/laracom/common/log"
-	"os"
-	"strings"
-	"time"
+	"log"
 )
 
 type DemoServiceHandler struct {
-	appConfig *AppConfig
+
 }
 
 func (s *DemoServiceHandler) SayHello(ctx context.Context, req *pb.DemoRequest, rsp *pb.DemoResponse) error {
-	// 从微服务上下文中获取追踪信息
-	md, ok := metadata.FromContext(ctx)
-	if !ok {
-		md = make(map[string]string)
-	}
-	var sp opentracing.Span
-	wireContext, _ := opentracing.GlobalTracer().Extract(opentracing.TextMap, opentracing.TextMapCarrier(md))
-	// 创建新的 Span 并将其绑定到微服务上下文
-	sp = opentracing.StartSpan("SayHello", opentracing.ChildOf(wireContext))
-	// 记录请求
-	sp.SetTag("req", req)
-	defer func() {
-		// 记录响应
-		sp.SetTag("res", rsp)
-		// 在函数返回 stop span 之前，统计函数执行时间
-		sp.Finish()
-	}()
-
 	// 如果参数为空设置默认值
 	if req.Name == "" {
-		req.Name = s.appConfig.UserName
+		req.Name = "学院君"
 	}
 	rsp.Text = "你好, " + req.Name
 	return nil
@@ -55,10 +24,8 @@ func (s *DemoServiceHandler) SayHello(ctx context.Context, req *pb.DemoRequest, 
 
 func (s *DemoServiceHandler) SayHelloByUserId(ctx context.Context, req *pb.HelloRequest, rsp *pb.DemoResponse) error {
 
-	// 使用断路器
-	hystrix.Configure([]string{"laracom.service.user.UserService.GetById"})
-	service := micro.NewService(
-		micro.WrapClient(hystrix.NewClientWrapper()),
+	service := k8s.NewService(
+		micro.Name("laracom.service.demo"),
 	)
 	client := userpb.NewUserServiceClient("laracom.service.user", service.Client())
 	resp, err := client.GetById(context.TODO(), &userpb.User{Id: req.Id})
@@ -70,93 +37,14 @@ func (s *DemoServiceHandler) SayHelloByUserId(ctx context.Context, req *pb.Hello
 }
 
 func main()  {
-	// 获取viper配置实例
-	appConfig := initAppConfig()
 
-	// 初始化全局服务追踪
-	t, io, err := tracer.NewTracer(appConfig.ServiceName, os.Getenv("MICRO_TRACE_SERVER"))
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	defer io.Close()
-	opentracing.SetGlobalTracer(t)
-
-	// 注册服务名必须和 demo.proto 中的 package 声明一致
 	service := micro.NewService(
-		micro.Name(appConfig.ServiceName),
-		micro.WrapHandler(traceplugin.NewHandlerWrapper(opentracing.GlobalTracer())), // 基于 jaeger 采集追踪数据
+		micro.Name("laracom.service.demo"),
 	)
 	service.Init()
 
-	pb.RegisterDemoServiceHandler(service.Server(), &DemoServiceHandler{appConfig: appConfig})
+	pb.RegisterDemoServiceHandler(service.Server(), &DemoServiceHandler{})
 	if err := service.Run(); err != nil {
-		logrus.Fatalf("服务启动失败: %v", err)
+		log.Fatalf("服务启动失败: %v", err)
 	}
-}
-
-func initAppConfig() *AppConfig {
-	// 默认使用 JSON 编码
-	encoder := json.NewEncoder()
-	fileSource := file.NewSource(file.WithPath("./demo.json"), source.WithEncoder(encoder))
-	etcdSource := etcd.NewSource(
-		etcd.WithAddress(strings.Split(os.Getenv("MICRO_REGISTRY_ADDRESS"), ",")[0]),
-		etcd.WithPrefix("laracom/demo/"),
-		source.WithEncoder(encoder),
-	)
-	conf := config.NewConfig()
-	var err error
-	if os.Getenv("ENABLE_REMOTE_CONFIG") == "true" {
-		err = conf.Load(
-			fileSource,  // 将文件配置作为默认值
-			etcdSource,  // 会覆盖上面的文件配置
-		)
-	} else {
-		err = conf.Load(fileSource)
-	}
-	if err != nil {
-		// 加载数据源失败
-		logrus.Fatalf("读取配置失败: %v", err)
-	}
-	var appConfig AppConfig
-	err = conf.Get("laracom", "demo", "app").Scan(&appConfig)
-	if err != nil {
-		// 读取远程配置失败
-		logrus.Fatalf("读取配置失败: %v", err)
-	}
-	logrus.Printf("初始化配置：%v", appConfig)
-	logrus.Printf("初始化配置：%v", conf.Map())
-
-	// 开启协程监听配置变更
-	go func(){
-		for {
-			time.Sleep(time.Second * 5) // delay after each request
-
-			w, err := conf.Watch("laracom", "demo", "app")
-			if err != nil {
-				logrus.Printf("监听配置变更失败: %v", err)
-				continue
-			}
-
-			// wait for next value
-			value, err := w.Next()
-			if err != nil {
-				logrus.Printf("读取配置变更失败: %v", err)
-				continue
-			}
-
-			value.Scan(&appConfig)
-			logrus.Printf("配置值变更：%s", &appConfig)
-		}
-	}()
-
-	return &appConfig
-}
-
-type DemoConfig struct {
-	App AppConfig `json:"app"`
-}
-
-type AppConfig struct {
-	ServiceName string `json:"service_name"`
-	UserName string `json:"user_name"`
 }
